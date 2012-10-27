d53407d1ac78810067a81c2bc6db403f908407f4
Mat <madrat.ifa@gmail.com>
2012-10-27 23:02:24 +0200

    Revert "Update parser-light.c"
    This reverts commit 6524a8b7fb9124cfac9f0b9b4f93be65c2c48cd6.

--- parser-light.c
+++ parser-light.c
@@ -1,4 +1,4 @@
-/* POKUS HOVNO Jen takovy test jestli to vazne funguje tak jak nam tvrdili :).
+/* Jen takovy test jestli to vazne funguje tak jak nam tvrdili :).
 Vsechno co tu mam bude v realu vypadat jinak, ale zakladni pointa je takovahle,
 aspon tak to chapu ja. Pro lepsi predstavu jak to bude vypadat kouknete na link
 z changelogu. Jakekoliv pripominky mi hned piste, Matej.
